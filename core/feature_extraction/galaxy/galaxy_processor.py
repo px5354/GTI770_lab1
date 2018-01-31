38895d1ff31a54c7a6d584ba320a1d78fff85551
Samuel Gervais <am03800@ens.etsmtl.ca>
2018-01-31 15:37:26 -0500

    clean processor

--- core/feature_extraction/galaxy/galaxy_processor.py
+++ core/feature_extraction/galaxy/galaxy_processor.py
@@ -6,13 +6,15 @@ Course :
     GTI770 — Systèmes intelligents et apprentissage machine
 
 Project :
-    Lab # X - Lab's name
+    Lab # 1 - Extraction de primitives
 
 Students :
-    Names — Permanent Code
+    ARRON VUONG     -   VUOA09109300
+    PHILIPPE LE     -   LEXP12119302
+    SAMUEL GERVAIS  -   GERS04029200
 
 Group :
-    GTI770-H18-0X
+    GTI770-H18-02
 """
 
 import cv2
@@ -467,49 +469,47 @@ class GalaxyProcessor(object):
 
         return np.array([blue_histogram, green_histogram, red_histogram])
 
-    def gerv_shits(self, image):
-        img_gray = self.get_gray_float_image(image)
-        image = self.remove_starlight(image, img_gray)
-        image = self.crop_image_with_extremes(image)
-        cv2.imwrite(os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/image.jpg", image)
-
-        img = cv2.imread(os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/image.jpg")
-        thresh = self.white_image(img)
-        cv2.imwrite(os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/thresh.jpg", thresh)
-
-        # ret, thresh = cv2.threshold(thresh, 127, 255, 0)
-        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        cnt = contours[0]
-
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(img, [box], 0, (124, 252, 0), 2)
-
-        cv2.imwrite(os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/img_coutour.jpg", img)
-
     def white_image(self, image):
+        """ Change the colors of an image.
+
+        Using openCV method to change colors from an image.
+
+        Args:
+            image: an OpenCV standard image format.
+
+        Returns:
+            The image in black and white a.k.a gray scale.
+        """
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
-        # threshold the image, then perform a series of erosions +
-        # dilations to remove any small regions of noise
         thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.erode(thresh, None, iterations=2)
         thresh = cv2.dilate(thresh, None, iterations=2)
 
         return thresh
 
-    def crop_image_with_extremes(self, image):
+    def crop_image_with_extremes(self, image, extraspace):
+        """ Crop the image with extremes values.
 
-        # load the image, convert it to grayscale, and blur it slightly
-        white_image = self.white_image(image)
+        Using openCV filters and thresh to help find contours.
+        Using openCV contours to detect the shape.
+        Using openCV method to crop the image from the extremes values from left, right, top and bottom.
 
-        # find contours in thresholded image, then grab the largest
-        # one
-        cnts = cv2.findContours(white_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        Args:
+            image: an OpenCV standard image format.
+            extraSpace: a value to add to the extremes.
+
+        Returns:
+            The image cropped from extremes values.
+        """
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+
+        cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[1]
         c = max(cnts, key=cv2.contourArea)
 
@@ -519,32 +519,24 @@ class GalaxyProcessor(object):
         extTop = tuple(c[c[:, :, 1].argmin()][0])
         extBot = tuple(c[c[:, :, 1].argmax()][0])
 
-        # draw the outline of the object, then draw each of the
-        # extreme points, where the left-most is red, right-most
-        # is green, top-most is blue, and bottom-most is teal
-        # cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
-        # cv2.circle(image, extLeft, 4, (0, 0, 255), -1)
-        # cv2.circle(image, extRight, 4, (0, 255, 0), -1)
-        # cv2.circle(image, extTop, 4, (255, 0, 0), -1)
-        # cv2.circle(image, extBot, 4, (255, 255, 0), -1)
+        lenght_DimX = len(image)
+        lenght_DimY = len(image[0])
 
-        points = image
-        lenght_DimX = len(points)
-        lenght_DimY = len(points[0])
-
-        x1 = extLeft[0] - 10
+        #extraSpace is used here because sometimes we want more than the fit size of the shape.
+        #So, we verify to make sure it's not going outside of the screen.
+        x1 = extLeft[0] - extraspace
         if x1 < 0:
             x1 = extLeft[0]
 
-        x2 = extRight[0] + 10
+        x2 = extRight[0] + extraspace
         if x2 > lenght_DimX:
             x1 = extRight[0]
 
-        y1 = extTop[1] - 10
+        y1 = extTop[1] - extraspace
         if y1 < 0:
             y1 = extTop[1]
 
-        y2 = extBot[1] + 10
+        y2 = extBot[1] + extraspace
         if y2 > lenght_DimY:
             y2 = extBot[1]
 
@@ -552,23 +544,174 @@ class GalaxyProcessor(object):
 
         return image
 
-    def correlation_image(self, image):
+    def correlation(self, image):
+        """ Give the correlation of the image.
+
+        Using numpy corrcoef methods to give the correlation between two array of data.
+
+        Args:
+            image: a black and white image.
+
+        Returns:
+            The correlation of white in the image.
+        """
+
         points = image.astype('float')
 
-        lenght_DimX = len(points)
-        lenght_DimY = len(points[0])
+        rows, cols = image.shape
 
         listX = []
         listY = []
 
-        for x in range(0, lenght_DimX - 1):
-            for y in range(0, lenght_DimY - 1):
-                if points[x][y] > 250:
+        for x in range(rows):
+            for y in range(cols):
+                if points[x][y] == 255:
                     listX.append(x)
                     listY.append(y)
 
         correlation = np.corrcoef(listX, listY)[0, 1]
+
         return correlation
+
+    def remove_little_shapes(self, image):
+        """ Removing little shapes in the image.
+
+        Using OpenCV connectedComponentsWithStats to get all the shapes
+
+        Args:
+            image: a black and white image.
+
+        Returns:
+            The image with one shape and nothing around it.
+        """
+
+        components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
+        sizes = stats[1:, -1];
+
+        img2 = np.zeros((output.shape))
+
+        for i in range(0, components - 1):
+            if sizes[i] == max(sizes):
+                img2[output == i + 1] = 255
+
+        return img2
+
+    def black_proportion(self, image):
+        """ GIve proportion of black in the image.
+
+        Using some image proporties to calculate the proportion
+
+        Args:
+            image: a black and white image.
+
+        Returns:
+            The proportion value of black in %.
+        """
+
+        points = image.astype('float')
+        rows, cols = image.shape
+        black = 0
+        count = 0
+
+        for x in range(rows):
+            for y in range(cols):
+                count += 1
+                if points[x][y] == 0:
+                    black += 1
+
+        proportion = black / count * 100
+
+        return proportion
+
+    def circularity(self, image):
+        """ Give the circularity of the shape in the image.
+
+        Using OpenCV findContours to get the shape of the image.
+        Using OpenCV contourArea to get the area.
+        Using OpenCV arcLength to get the perimeters of the shape.
+
+        Args:
+            image: an OpenCV standard image format.
+
+        Returns:
+            The circularity value of the shape in the image in %.
+        """
+
+        white_image = self.white_image(image)
+        im2, contours, hierarchy = cv2.findContours(white_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnt = contours[0]
+        area = cv2.contourArea(cnt)
+        perimeter = cv2.arcLength(cnt, True)
+
+        circularity = (4 * math.pi * area) / (perimeter * perimeter) * 100
+
+        return circularity
+
+    def feature_circularity(self, image):
+        """ Feature to give the circularity
+
+        Using home made methods to prepare the image.
+
+        Args:
+            image: an OpenCV standard image format.
+
+        Returns:
+            The circularity value in %.
+        """
+
+        temp_path = os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/temp_image.jpg"
+        original_image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
+        cropped_img = self.crop_image_with_extremes(original_image, 0)
+        white_image = self.white_image(cropped_img)
+        clean_image = self.remove_little_shapes(white_image)
+        cv2.imwrite(temp_path, clean_image)
+        final_image = cv2.imread(temp_path)
+        circularity = self.circularity(final_image)
+        cv2.imwrite(os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/circularity_image.jpg", final_image)
+
+        return circularity
+
+    def feature_correlation(self, image):
+        """ Feature to give the correlation
+
+        Using home made methods to prepare the image.
+
+        Args:
+            image: an OpenCV standard image format.
+
+        Returns:
+            The correlation value.
+        """
+
+        original_image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
+        cropped_img = self.crop_image_with_extremes(original_image, 0)
+        white_image = self.white_image(cropped_img)
+        clean_image = self.remove_little_shapes(white_image)
+        correlation = self.correlation(clean_image)
+
+        cv2.imwrite(os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/correlation_image.jpg", clean_image)
+
+        return correlation
+
+    def feature_black_proportion(self, image):
+        """ Feature to give the black proportion
+
+        Using home made methods to prepare the image
+
+        Args:
+            image: an OpenCV standard image format.
+
+        Returns:
+            The black proportion value in %.
+        """
+
+        original_image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
+        cropped_img = self.crop_image_with_extremes(original_image, 0)
+        white_image = self.white_image(cropped_img)
+        clean_image = self.remove_little_shapes(white_image)
+        proportion = self.black_proportion(clean_image)
+
+        return proportion
 
     def get_features(self, image_file, id, label):
         """ Get the image's features.
@@ -586,15 +729,28 @@ class GalaxyProcessor(object):
 
         print("Processing file : " + image_file)
 
+        original_image = cv2.imread(filename=image_file)
+
+        circularity = self.feature_circularity(original_image)
+        correlation = self.feature_correlation(original_image)
+        proportion = self.feature_black_proportion(original_image)
+
+        print("CIRCULARIY: " + repr(circularity))
+        print("CORRELATION: " + repr(correlation))
+        print("PROPORTION: " + repr(proportion) +"%")
+        print("LABEL: " + repr(label))
+
         # Declare a list for storing computed features.
         features = list()
         features1 = list()
         features2 = list()
 
         img_color = cv2.imread(filename=image_file)
+
+        ######### PHIL ET ARRON #########
         height, width, dim = img_color.shape
 
-        clean_img = self.crop_image_with_extremes(img_color)
+        clean_img = self.crop_image_with_extremes(img_color, 0)
         # clean_img = self.remove_starlight(clean_img, self.get_gray_image(clean_img))
         not_cropped_img = self.remove_starlight(img_color, self.get_gray_image(img_color))
         cv2.imwrite(os.environ["VIRTUAL_ENV"] + "/data/csv/galaxy/before_img.jpg", clean_img)
@@ -647,4 +803,8 @@ class GalaxyProcessor(object):
         # sat_img = self.saturate(img_color, 0.95, 1)
         # cv2.imwrite(os.environ["VIRTUAL_ENV"] +"/data/csv/galaxy/sat_img.jpg", sat_img)
         # return features
+
+        ######### PHIL ET ARRON #########
+
+
         return features1, features2, label
